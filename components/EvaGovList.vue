039fb73065ceb742e0e1d63e49dfb2b47fececde
anapaarmavir777 <vitya.lantuh@yandex.ru>
2023-04-07 16:20:42 +0300

    update

--- components/EvaGovList.vue
+++ components/EvaGovList.vue
@@ -1,47 +1,77 @@
 <template>
-   <v-list >
-    <v-list-item v-on:click="go" v-for="gov in govs">
-        {{ gov.govnum }} <br>
-        {{ gov.dt }} <br>
-        {{ gov.id }}
-    </v-list-item>
-   </v-list>
+    <div class="eva-govs">
+        <v-list v-if="(govs.length > 0)">
+            <v-list-item v-for="gov in govs"
+                          :key="gov.id"
+                          v-on:click="go(gov.id)">
+                <v-list-item-title>
+                    {{ gov.govnum }}
+                    <div class="dt">
+                        {{ moment(gov.dt).format('DD.MM.YYYY HH:mm') }}
+                    </div>
+                </v-list-item-title>
+                <v-list-item-action>
+                    <v-icon>mdi-chevron-right</v-icon>
+                </v-list-item-action>
+             </v-list-item>
+        </v-list>
+    </div>
 </template>
 <script>
-
 const _LS_KEY = 'saved-govs';
+const moment = require("moment");
+moment.locale('ru');
 
 export default {
     name: 'EvaGovList',
     data(){
-        const govs = [{
-            govnum: 1,
-            dt: new Date(),
-            id: 10000   
-        }];
-
-
         return { 
-            govs
+            moment
         };
     },
 
-    created(){
-        this.govs = JSON.parse(localStorage.getItem(_LS_KEY)) || [];
-    },
-
     methods:{
-        go(gov){
-            gov.dt = new Date();
-            localStorage.setItem('govs', JSON.stringify(this.govs))
-            this.$emit('go', gov);
+        go(id){
+            const n = this.govs.findIndex( gov => gov.id === id);
+            this.govs[n].dt = new Date();
+             this.govs[q] = this.$store.getters["settings/govs"];
+            this.$emit('go', this.govs[n]);
         },
         
         save(gov){
-            gov.dt = new Date();
-            this.govs.push(gov);
-            localStorage.setItem(_LS_KEY, JSON.stringify(this.govs));
+            const n = this.govs.findIndex( _gov => _gov.id === gov.id );
+            if ( n < 0 ) {
+                gov.dt = new Date();
+                this.govs.push(gov);
+            } else {
+                this.govs[n].dt = new Date();
+            }
+            this.govs[q] = this.$store.getters["settings/govs"];
         }
     },
+    computed: {
+        govs(){
+            return this.$store.getters["settings/govs"]
+
+        }
+    }
 }
 </script>
+<style lang="scss">
+.eva-govs{
+    & .v-list{
+        &-item{
+            padding-bottom: 0.5rem;
+            &__title{
+                font-size: 2rem !important;
+                text-transform: uppercase;
+                & .dt{
+                    color: #666;
+                    font-size: 1rem !important;
+                    font-weight: 300;
+                }
+            }
+        }
+    }
+}
+</style>
